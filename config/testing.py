4fd2ed2cc1823cd838046286295f845069c6a0cf
Lorena Gomez <lorena.natali18@gmail.com>
2023-01-18 11:48:47 -0300

    git ignore modif

--- config/testing.py
+++ config/testing.py
@@ -1,7 +1,7 @@
 SECRET_KEY = 'testeodeapidepytohn'
 
 MYSQL_HOST = 'localhost'
-MYSQL_USER = ''
-MYSQL_PASSWORD = ''
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'root'
 MYSQL_DB = 'dbtesting'
 MYSQL_PORT = 3306
